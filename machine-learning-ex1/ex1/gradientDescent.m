1b51372a678fb8a8ab79fe3206fbf4971f968a6f
Tom Martin <tom@tpgmartin.com>
2017-02-16 09:51:02 +0000

    Complete week 2 programming assignment

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -17,11 +17,14 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
-
-
-
-
+    H = X * theta
+    T = [0 ; 0];
+    
+    for i = 1 : m,
+      T += (H(i) - y(i)) * X(i,:)';
+    end
+    
+    theta -= (alpha * T) / m;
 
     % ============================================================
 
